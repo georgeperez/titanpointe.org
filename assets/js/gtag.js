024515519d5b68665b431d19598bbd66238524d2
George Perez <george.perez.marrero@gmail.com>
2018-01-20 20:36:17 -0400

    This should definitely fix it

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,8 +1,6 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40381065-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-40381065-2');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-40381065-2');
