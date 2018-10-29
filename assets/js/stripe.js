1cf61e67e71066425a5ae38a31fb51529d93bb2c
George Perez <george.perez.marrero@gmail.com>
2018-10-29 12:42:26 -0400

    Initial Stripe Elements integration

---
+++ assets/js/stripe.js
@@ -0,0 +1,51 @@
+var stripe = Stripe('pk_test_GKv7j1PL1GLp0ew3ZtQfCv6O');
+var elements = stripe.elements();
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '18px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+
+var card = elements.create('card', {style: style});
+
+
+card.mount('#card-element');
+
+
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+
+      stripeTokenHandler(result.token);
+    }
+  });
+});
